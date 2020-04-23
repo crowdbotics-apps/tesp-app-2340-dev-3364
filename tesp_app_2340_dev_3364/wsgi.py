f378b11a043b66ccc91715c11ba2255b7e0741d2
Crowdbotics <team@crowdbotics.com>
2020-04-23 08:03:40 +0000

    Update models

--- tesp_app_2340_dev_3364/wsgi.py
+++ tesp_app_2340_dev_3364/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tesp_app_2340_dev_3364.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tesp_app_2340_dev_3364.settings")
 
 application = get_wsgi_application()
